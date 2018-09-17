9d31c6232c95bc782f12ef91410607b9c420342e
Alice.Lee <alice.cy.lee@getac.com.tw>
2018-09-17 10:21:29 +0800

    mytrips copy

---
+++ MySite/myenv/mysite/views.py
@@ -0,0 +1,9 @@
+from django.http import HttpResponse
+from datetime import datetime
+from django.shortcuts import render
+
+def hello_world(request):
+	return render(request, 'hello_world.html', 
+		{'current_time': str(datetime.now()),
+		})
+	
---
+++ MySite/myenv/mytrip/trips/admin.py
@@ -0,0 +1,10 @@
+from django.contrib import admin
+from .models import Post
+
+# Register your models here.
+# admin user: alice.cy.lee // pw: alice.cy.lee
+
+
+
+
+admin.site.register(Post)
